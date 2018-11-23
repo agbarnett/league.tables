ce2558e24c0ced84f65b90ce66ba899e435b14dc
Adrian Barnett <a.barnett@qut.edu.au>
2018-11-23 19:39:06 +1000

    Add shiny code

---
+++ server.R
@@ -0,0 +1,45 @@
+# server for league tables
+# July 2018
+
+shinyServer(function(input, output) {
+  
+  # get the data
+  load('data/Processed.papers.for.Shiny.RData')
+
+  # function to get filtered papers (used by basics and table; must be copied into report)
+  my.filter = function(indata=results){
+    res = indata
+    
+    # filter on year
+    res = filter(res, year==input$year)
+    res$overall = res$rank
+
+    # filter on country
+    if(input$country != 'All'){
+      index = grep(input$country, res$country)
+      res = res[index,]
+      res$rank = 1:nrow(res)
+    }
+
+    # filter on region
+    if(input$region != 'World'){
+      index = grep(input$region, res$Region)
+      res = res[index,]
+      res$rank = 1:nrow(res)
+    }
+    
+    stats = dplyr::mutate(res, interval = paste(round(rlower), ' to ', round(rupper), sep='')) %>%
+        select(overall, interval, rank, affiliation, wsum, cluster.member) %>%
+        rename('Overall rank'=overall, '95% interval'=interval, 'Rank'=rank, 'Affiliation'=affiliation,
+               'Score'=wsum, 'Cluster' = cluster.member)
+
+    return(stats)
+  }
+
+  # table of rankings:
+  output$table <- renderTable({
+    papers = my.filter()
+    papers
+  }, digits =0)
+  
+})
---
+++ ui.R
@@ -0,0 +1,63 @@
+# UI for league tables
+# Nov 2018
+
+shinyUI(fluidPage(
+  
+  # Application title
+  tags$h2("A university league table based on good research practice"),
+  div(p(HTML(paste0("This international league table is based on the ", 
+tags$a(href="https://www.equator-network.org/", "EQUATOR guidelines"))))),
+  p("Universities were awarded points based on the number of times their staff cited an appropriate EQUATOR guideline.
+    We used the EQUATOR guidelines for randomised trials (CONSORT), systematic reviews (PRISMA) and observational studies (STROBE).
+    The tables focus on health and medical research."),
+  p("The tables show the score and rank based on the score. We also show the uncertainty in overall rank using a 95% bootstrap confidence interval. The cluster column is an attempt to group universities with a similar score using five clusters with 5 as the best."),
+  p("We have included 'Missing' as a separate university, which combines all the affiliation data that was vague (e.g., `School of Medicine`) or missing."),
+  p('The citation data came from Scopus.'),
+  
+  sidebarLayout(
+    sidebarPanel(
+
+      numericInput(inputId = "year",
+                              label = "Year:",
+                              min = 2016,
+                              max = 2017,
+                              step = 1,
+                              value = 2017),
+      
+      radioButtons(inputId = "country", 
+                   label = "Country:",
+                   choices = c("All" = "All",
+                               "Australia" = "Australia",
+                               "Brazil" = "Brazil",
+                               "China" = 'China',
+                               "Canada" = 'Canada',
+                               "Germany" = "Germany",
+                               "Italy" = "Italy",
+                               "Japan" = "Japan",
+                               "Netherlands" = "Netherlands",
+                               "New Zealand" = "New Zealand",
+                               "Spain" = "Spain",
+                               "UK" = 'United Kingdom',
+                               "USA" = 'United States'
+                               ), 
+                   selected = 'All'),
+      
+      radioButtons(inputId = "region", 
+                   label = "Region:",
+                   choices = c("World" = "World",
+                               "Africa" = 'AFRICA',
+                               "Asia" = 'ASIA',
+                               "Europe" = 'EUROPE',
+                               "Latin America and Caribbean" = 'LATIN AMER',
+                               "North America" = "NORTHERN AMERICA",
+                               "Oceania" = 'OCEANIA'), 
+                   selected = 'World')
+
+    ), # end of sidebar panel
+    
+    mainPanel(
+    h3('Rankings'),
+    tableOutput(outputId = 'table')
+    ) # end of main panel
+
+)))
